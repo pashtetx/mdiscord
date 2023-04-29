a9f3ef6369d5d285f0385dd6ac61e51231b92532
pashtetx <vadimplej43@gmail.com>
2023-04-29 16:33:15 +0300

    discordv2

---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = "NjY3ODAxNTg2NTYyNjI5NjQz.GzbzbD.qFGvkbof5mZJL7fmP1X3og8hgLMMf70tIYqz60"
+STATUS = "music"
--- main.py
+++ main.py
@@ -1,74 +1,54 @@
-from time import sleep
-from json import dumps
-import requests
-import datetime
-import random
+import requests, json
 
 
-class Main():
+class MDiscord:
 
 	def __init__(self, token, select):
 
-		self.headers = {"authorization": token, "content-type": "application/json"}
-		self.url = 'https://discord.com/api/users/@me/settings'
-		self.delay = 4
-
-		self.status_list = {
-			"time":self.time,
-			"loading":self.loading,
-			"ghoul":self.ghoul,
+		self.headers = {
+			"authorization": token,
+			"content-type": "application/json"
 		}
 
+		self.select = select
+		self.delay = 4
 
 
-		self.select_status(select)
+		self.statuses = {
 
-	def select_status(self, select: str) -> None:
-		for k, v in self.status_list.items():
-			if k.lower() == select:
-				v()
-				break
+		}
 
+	def _select_status(self, select: str): 
 
-	def get_delay(self):
+		if self.statuses == {}:
+			assert ValueError("Statuses not defined") # если статусы пустые
 
-		return random.randint(self.delay - 1, self.delay)
+		for name, status_func in self.statuses.items():
+			if select == name:
+				status_func(self.delay, self.set_status)
 
-	def set_status(self, status: str) -> None:
-		res = requests.patch(self.url, headers = self.headers, data = dumps({'custom_status': {'text':status}}))
-		print(res)
+	def set_status(self, status: str, emoji: str = ""):
 
- 
-	def ghoul(self): # ghoul
+		res = requests.patch(
+			"https://discord.com/api/users/@me/settings",
+			 headers = self.headers, 
+			 data = json.dumps(
+			 	{
+			 		"custom_status": {
+			 			'text':status,
+			 			"emoji_name": emoji
+			 		}
+			 	})
+			 )
 
-		i = 6
-
-		while i > 0:
-			self.set_status(f"{i} - 7 = {i - 7}")
-			i = i - 7
-			if i < 0:
-				i = 1000
-				self.set_status("Я гуль")
-			sleep(self.get_delay())
-
-	def loading(self): # loading
-		
-		i = 1
-
-		while i <= 10:
-			self.set_status(("#" * i) + "-" * (10 - i) + f" {i}0%")
-			sleep(self.get_delay())
-			i += 1
-			if i == 10:
-				i = 1
-				self.set_status("Какой-то текст")
-				sleep(self.get_delay())
+		if res.status_code != 200:
+			print("Ошибка:", res.status_code)
 
 
-	def time(self): # time
+	def add_status(self, func, name: str) -> None:
+		self.statuses[name] = func
 
-		while True:
-			print(self.get_delay())
-			self.set_status(datetime.datetime.now().strftime("%H:%M")) # Set status formated now datetime.
-			sleep(self.get_delay())
+
+	def run(self):
+		self._select_status(self.select)
 
